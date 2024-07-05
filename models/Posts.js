13269afe43b5c55c038b5b80327ed49dcb432de8
Michael Hellon <kansasboy308@gmail.com>
2024-07-05 16:10:56 -0500

    added files to refractor

---
+++ models/Posts.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts',
+  }
+);
+
+module.exports = Project;
