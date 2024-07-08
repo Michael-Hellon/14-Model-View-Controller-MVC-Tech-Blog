ebb1630bc277bae436f426e58714ac973ea42725
Michael Hellon <kansasboy308@gmail.com>
2024-07-08 18:11:59 -0500

    changed variable name Changes to be committed: modified:   seeds/seeds.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,19 +1,19 @@
 const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
 
-const seedUserData = require('./userData.json');
-const seedPostData = require('./postData.json');
+const userData = require('./userData.json');
+const postData = require('./postData.json');
 // const seedCommentData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(seedUserData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for ( const post of seedPostData) {
+  for ( const post of postData) {
     await Post.create({
       ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
