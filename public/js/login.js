819a7d99f6082ea22f0e1d3e6fbaec11823496b4
Michael Hellon <kansasboy308@gmail.com>
2024-07-11 23:04:06 -0500

    de buggin login Changes to be committed: 	modified:   public/js/login.js 	modified:   views/login.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -2,16 +2,16 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  // ('#name-login') for username login
+  // ('#email-login') for username login
   // ('.login-form') for event listener
-  const name = document.querySelector('#name-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (name && password) {
+  if (email && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ name, password }),
+      body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
