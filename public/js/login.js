ff82877230516816d932b774119aec0ef327b49b
Michael Hellon <kansasboy308@gmail.com>
2024-07-12 20:09:25 -0500

    more de buggin, trying to get login/sign up to work, and posts to render Changes to be committed: 	modified:   public/css/style.css 	modified:   public/js/login.js 	deleted:    public/js/signup.js 	modified:   utils/helpers.js 	modified:   views/dashboard.handlebars 	modified:   views/homepage.handlebars 	modified:   views/layouts/main.handlebars 	modified:   views/login.handlebars 	deleted:    views/signup.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -24,8 +24,38 @@ const loginFormHandler = async (event) => {
   }
 };
 
-// line 7 of login.handlebars
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the signup form
+  // ('#name-signup') for username sign up
+  // ('#email-signup') for username sign up
+  // ('.signup-form') for event listener
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Error!!! Unable to sign up new user!');
+    }
+  }
+};
+  
+// line 8 of login.handlebars
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
 
+  // line 30 of signup.handlebars
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
