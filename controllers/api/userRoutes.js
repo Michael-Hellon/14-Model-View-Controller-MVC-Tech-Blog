34df3008049fd5026cf2cf288ab81837349fa698
Michael Hellon <kansasboy308@gmail.com>
2024-07-19 16:28:40 -0500

    debugged everything. logout, all paths are good Changes to be committed: 	modified:   controllers/api/userRoutes.js 	modified:   seeds/postData.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -95,7 +95,7 @@ router.post('/login', async (req, res) => {
 
 // logout route
 router.post('/logout', (req, res) => {
-  if (!req.session.logged_in) {
+  if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
     });
--- seeds/postData.js
+++ seeds/postData.js
@@ -4,17 +4,17 @@ const postData = [
 
     {
         title: "Growing Zucchini",
-        content: "blah blah blah",
+        content: "Plant the seeds 1/2 an inch below the surface of a small hills in your garden. They grow best in direct sunlight. The soil needs to be rich in organic matter and well drained. They need at least 1 inch of water a week.  ",
         user_id : 1 
     },
     {
         title: "Brewing Cider",
-        content: "Clean everything really well. blah blah blah",
+        content: "After all your equipment is dry from being cleaned and sanitized. Mix 13, 12oz containers of frozen apple juice with 5 gallons of 80-90 degree F water. Then add 5 pounds of sugar and one packet of champagne yeast. Once fermented and bottled this will yield about 25-30 750 ml bottles of 15-18% ABC apple cider/wine",
         user_id : 2 
     },
     {
         title: "Building a Deck",
-        content: "First, make sure you get a permit blah blah blah",
+        content: "First, make sure you get a building permit from the proper authorities. Otherwise this causes delays and possible fines.",
         user_id: 3
     },
     {
