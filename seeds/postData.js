caa3438f4178c9f8bf651e68e49fc43ad3175c0d
Michael Hellon <kansasboy308@gmail.com>
2024-07-20 20:47:36 -0500

    fixed a few bug so HB display properly Changes to be committed: 	modified:   seeds/postData.js 	modified:   public/css/style.css 	modified:   public/js/login.js 	modified:   public/js/post-edit.js 	modified:   seeds/commentData.js 	modified:   seeds/postData.js 	modified:   views/dashboard.handlebars 	modified:   views/homepage.handlebars 	modified:   views/layouts/main.handlebars 	modified:   views/login.handlebars 	modified:   views/post-id.handlebars
    	public/images/

--- seeds/postData.js
+++ seeds/postData.js
@@ -4,12 +4,12 @@ const postData = [
 
     {
         title: "Growing Zucchini",
-        content: "Plant the seeds 1/2 an inch below the surface of a small hills in your garden. They grow best in direct sunlight. The soil needs to be rich in organic matter and well drained. They need at least 1 inch of water a week.  ",
+        content: "Plant the seeds 1/2 an inch below the surface of a small hills in your garden. They grow best in direct sunlight.",
         user_id : 1 
     },
     {
         title: "Brewing Cider",
-        content: "After all your equipment is dry from being cleaned and sanitized. Mix 13, 12oz containers of frozen apple juice with 5 gallons of 80-90 degree F water. Then add 5 pounds of sugar and one packet of champagne yeast. Once fermented and bottled this will yield about 25-30 750 ml bottles of 15-18% ABC apple cider/wine",
+        content: "Mix 13, 12oz containers of frozen apple juice with 5 gallons of 80-90 degree F water. Then add 5 pounds of sugar and one packet of champagne yeast, yield about 25-30 750 ml bottles of 15-18% ABC apple cider/wine",
         user_id : 2 
     },
     {
